23ef7d6d1847d59fd8bed80eb3dea23fda784a26
Yashwanth <yashwanth.alakky@gmail.com>
2021-02-10 10:32:52 +0530

    i3 title bar

--- .bashrc
+++ .bashrc
@@ -16,3 +16,19 @@ export PS1="\[\033[01;37m\][\[\033[01;33m\]\u\[\033[01;31m\]@\[\033[01;32m\]\H \
 [[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx
 
 echo "PATH=$PATH:~/.config/rofi/bin" >> ~/.profile
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/yash/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/yash/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/yash/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/yash/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
---
+++ .config/i3/lock.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+B='#00000000'  # blank
+C='#ffffff22'  # clear ish
+D='#ff00ffcc'  # default
+T='#ee00eeee'  # text
+W='#880000bb'  # wrong
+V='#bb00bbbb'  # verifying
+
+./x86_64-pc-linux-gnu/i3lock \
+--insidevercolor=$C   \
+--ringvercolor=$V     \
+\
+--insidewrongcolor=$C \
+--ringwrongcolor=$W   \
+\
+--insidecolor=$B      \
+--ringcolor=$D        \
+--linecolor=$B        \
+--separatorcolor=$D   \
+\
+--verifcolor=$T        \
+--wrongcolor=$T        \
+--timecolor=$T        \
+--datecolor=$T        \
+--layoutcolor=$T      \
+--keyhlcolor=$W       \
+--bshlcolor=$W        \
+\
+--screen 1            \
+--blur 5              \
+--clock               \
+--indicator           \
+--timestr="%H:%M:%S"  \
+--datestr="%A, %m %Y" \
+--keylayout 1         \
+
+# --veriftext="Drinking verification can..."
+# --wrongtext="Nope!"
+# --textsize=20
+# --modsize=10
+# --timefont=comic-sans
+# --datefont=monofur
+# etc
+
